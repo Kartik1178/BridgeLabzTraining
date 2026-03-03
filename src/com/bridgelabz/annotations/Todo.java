b5f6ebf17ed68b929d6500a45e429ac3efbdaa4d
Kartikeya Pusapati <swagboss600@gmail.com>
2026-03-03 09:35:16 +0530

    [Kartikeya]: solved annotations problems

---
+++ src/com/bridgelabz/annotations/Todo.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * Custom annotation example.
+ * - @Target: can be used on methods and types (classes)
+ * - @Retention: available at runtime for reflection
+ */
+@Documented
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface Todo {
+    String author() default "Kartikeya";
+    String description();
+    Priority priority() default Priority.MEDIUM;
+
+    enum Priority { LOW, MEDIUM, HIGH }
+}
